5d088902686085eb0d886326732dc779992ed2b1
Marco Uldemolíns <zmarcou2006@gmail.com>
2025-05-05 10:57:34 +0200

    Mesa

---
+++ src/desafiorestaurante/Mesa.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafiorestaurante;
+
+/**
+ *
+ * @author Marco
+ */
+public class Mesa {
+    private int numero;
+    private boolean ocupada;
+    
+    public Mesa(){}
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the ocupada
+     */
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    /**
+     * @param ocupada the ocupada to set
+     */
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+    
+}
