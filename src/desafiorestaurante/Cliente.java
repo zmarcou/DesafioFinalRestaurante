d0160428e7dcdf324d770c1f62480760273e18a8
Marco Uldemolíns <zmarcou2006@gmail.com>
2025-05-05 10:56:49 +0200

    Cliente

---
+++ src/desafiorestaurante/Cliente.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafiorestaurante;
+
+/**
+ *
+ * @author Marco
+ */
+public class Cliente {
+    private int id;
+    private String nombre;
+    
+    public Cliente(){}
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
