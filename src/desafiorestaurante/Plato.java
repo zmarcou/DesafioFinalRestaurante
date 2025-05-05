9d524ebbfeec0acc84640b438735ddbfba625e03
Marco Uldemolíns <zmarcou2006@gmail.com>
2025-05-05 10:58:14 +0200

    Plato

---
+++ src/desafiorestaurante/Plato.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desafiorestaurante;
+
+/**
+ *
+ * @author Marco
+ */
+public class Plato {
+    private int id;
+    private String nombre;
+    private double precio;
+    
+    public Plato(){}
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
